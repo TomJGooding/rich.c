9dc486ef2fee9ced34f39f5bfd0105453c55584b
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2024-08-11 11:39:22 +0100

    feat: add color from rgb hex

--- rich.c
+++ rich.c
@@ -1,6 +1,8 @@
+#include <assert.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef enum {
     DEFAULT,
@@ -30,6 +32,46 @@ typedef struct {
 
 Color color_from_rgb(int red, int green, int blue) {
     Color color = {.type = TRUECOLOR, .triplet = {red, green, blue}};
+    return color;
+}
+
+Color color_from_rgb_hex(char *hex_string) {
+    if (strlen(hex_string) != 6) {
+        fprintf(
+            stderr,
+            "Error: Invalid hex color code '%s': Must be six characters.\n",
+            hex_string
+        );
+        exit(EXIT_FAILURE);
+    }
+
+    Color color = {.type = TRUECOLOR};
+    char slice[3] = {0};
+    char *endptr;
+
+    for (int i = 0; i < 6; i += 2) {
+        strncpy(slice, hex_string + i, 2);
+        long value = strtol(slice, &endptr, 16);
+
+        if (*endptr) {
+            fprintf(
+                stderr,
+                "Error: Invalid hex color code '%s': Invalid character '%s'.\n",
+                hex_string,
+                endptr
+            );
+            exit(EXIT_FAILURE);
+        }
+
+        if (i == 0) {
+            color.triplet.red = (int)value;
+        } else if (i == 2) {
+            color.triplet.green = (int)value;
+        } else if (i == 4) {
+            color.triplet.blue = (int)value;
+        }
+    }
+
     return color;
 }
 
@@ -77,7 +119,7 @@ void rich_print(Text text) {
 int main() {
     Style style = {
         .color = color_from_rgb(255, 0, 0),
-        // .bgcolor = color_from_rgb(255, 255, 0)
+        .bgcolor = color_from_rgb_hex("ffff00")
     };
     Text text = {"Hello, World!\n", style};
 
